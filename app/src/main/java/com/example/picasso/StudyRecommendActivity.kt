210872bae6114786f579bb0fc250bac53b6c57fd
Inhyoe <melon7256@naver.com>
2023-02-07 14:58:09 +0900

    [feat] practice Logout

---
+++ app/src/main/java/com/example/picasso/StudyRecommendActivity.kt
@@ -0,0 +1,45 @@
+package com.example.picasso
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+class StudyRecommendActivity : AppCompatActivity() {
+    // 로그아웃 구현을 위한 변수
+    var auth : FirebaseAuth?= null
+    var googleSignInClient : GoogleSignInClient?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_study_recommend)
+
+        // 구글 로그아웃을 위해 로그인 세션 가져오기
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // firebaseauth를 사용하기 위한 인스턴스 get
+        auth = FirebaseAuth.getInstance()
+
+        // 구글 로그아웃 버튼 ID 가져오기
+        var google_sign_out_button = findViewById<Button>(R.id.google_sign_out_button)
+
+        // 구글 로그아웃 버튼 클릭 시 이벤트
+        google_sign_out_button.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            googleSignInClient?.signOut()
+
+            var logoutIntent = Intent (this, MainActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(logoutIntent)
+        }
+
+    } //onCreate
+}
