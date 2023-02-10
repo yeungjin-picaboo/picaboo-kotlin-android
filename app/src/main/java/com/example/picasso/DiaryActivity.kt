f74bc907e24c018a4c25e852cb111c02cff8a891
Inhyoe <melon7256@naver.com>
2023-02-10 15:09:07 +0900

    [feat] Add Res & Setting

--- app/src/main/java/com/example/picasso/DiaryActivity.kt
+++ app/src/main/java/com/example/picasso/DiaryActivity.kt
@@ -8,6 +8,7 @@ import android.view.animation.Animation
 import android.view.animation.AnimationSet
 import android.view.animation.AnimationUtils
 import androidx.appcompat.widget.AppCompatImageButton
+import androidx.core.view.isGone
 import com.example.picasso.databinding.ActivityDiaryBinding
 
 class DiaryActivity : AppCompatActivity() {
@@ -26,22 +27,36 @@ class DiaryActivity : AppCompatActivity() {
         val animation2 = AnimationUtils.loadAnimation(this, R.anim.updonw_reverse)
         val rolling = AnimationUtils.loadAnimation(this, R.anim.rolling_btn)
         val rolling2 = AnimationUtils.loadAnimation(this, R.anim.rolling_btn_reverse)
+        val opacity_cal_reverse = AnimationUtils.loadAnimation(this,R.anim.opacity_cal_reverse)
 
-        val testView = binding.testView
+        val calendarView = binding.calendarView
         val showCal = binding.showCal
-
-        testView.visibility = View.INVISIBLE
+        val whiteView = binding.WhiteView
+        val textView = binding.EditText02
+//        testView.visibility = View.INVISIBLE
         var state: Boolean = true
 
+        showCal.bringToFront()
         binding.showCal.setOnClickListener {
             if (state) {
+//                var s = AnimationSet(false)
+//                s.addAnimation(opacity)
+//                s.addAnimation(animation)
+//                animationSet객체를 사용하면 xml의 fillAfter이 적용되지 않는 버그가 있음
+
                 state = rollingBtn(showCal as AppCompatImageButton, state, rolling as AnimationSet)
-                testView.visibility = View.VISIBLE
-                testView.startAnimation(animation)
+                whiteView.startAnimation(animation)
+                textView.startAnimation(opacity_cal_reverse)
+                binding.root.removeView(textView)
+
+
             } else {
                 state = rollingBtn(showCal as AppCompatImageButton, state, rolling2 as AnimationSet)
-                testView.startAnimation(animation2)
-                testView.visibility = View.INVISIBLE
+                Log.d("state","$state")
+                whiteView.startAnimation(animation2)
+                binding.root.addView(textView)
+                whiteView.visibility = View.VISIBLE
+
             }
         }
     }
@@ -53,9 +68,11 @@ class DiaryActivity : AppCompatActivity() {
     ): Boolean {
         if (state) {
             wiget.startAnimation(animation)
+            Log.d("se","$state")
             return !state
         } else {
             wiget.startAnimation(animation)
+            Log.d("sef","$state")
             return !state
         }
     }
