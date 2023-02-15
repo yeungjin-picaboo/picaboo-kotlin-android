ebd067ddb750ed70cd0882f4844eb1d2fb383091
Inhyoe <melon7256@naver.com>
2023-02-16 06:14:12 +0900

    [feat] Update img & button

--- app/src/main/java/com/example/picasso/WeatherMoodActivity.kt
+++ app/src/main/java/com/example/picasso/WeatherMoodActivity.kt
@@ -3,23 +3,21 @@ package com.example.picasso
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageButton
+import java.lang.NumberFormatException
 import com.example.picasso.databinding.ActivityWeatherMoodBinding
+class WeatherMoodActivity : AppCompatActivity(), View.OnClickListener {
 
-class WeatherMoodActivity : AppCompatActivity() {
-
-    var selectedButton:ImageButton? = null
     var scaleToBig: Animation ?= null
     var scaleToSmall:Animation ?= null
     private val binding by lazy {
         ActivityWeatherMoodBinding.inflate(layoutInflater)
     }
-    private var selected = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        scaleToBig = AnimationUtils.loadAnimation(this, R.anim.btn_to_big_scale)
-        scaleToSmall = AnimationUtils.loadAnimation(this, R.anim.btn_to_small_scale)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         val snow = binding.imageButtonSnow
@@ -27,58 +25,96 @@ class WeatherMoodActivity : AppCompatActivity() {
         val sunny = binding.imageButtonSunny
         val cloudy = binding.imageButtonCloudy
         val windy = binding.imageButtonWindy
-
-
-        val imageArray = arrayOf<ImageButton>(
+        val happy = binding.imageButtonhappy
+        val good = binding.imageButtonGood
+        val netural = binding.imageButtonNetural
+        val bad = binding.imageButtonBad
+        val confused = binding.imageButtonConfused
+        val angry = binding.imageButtonAngry
+        val nervous = binding.imageButtonNervous
+        val sad = binding.imageButtonSad
+        val sick = binding.imageButtonSick
+        val weatherImageArray = arrayOf<ImageButton>(
             sunny,rainy,snow,cloudy,windy
         )
+        val moodImageArray = arrayOf<ImageButton>(
+            happy,good,netural,bad,confused,angry,nervous,sad,sick
+        )
+        for (i in weatherImageArray.indices){
+            weatherImageArray[i].setOnClickListener(this)
+        }
+        for (i in moodImageArray.indices){
+            moodImageArray[i].setOnClickListener(this)
+        }
 
-        snow.setOnClickListener {
-//            if (selected) {
-//                snow.startAnimation(scaleToBig)
-//                snow.setSelected(selected)
-//
-//                selected = false
-//                Log.d("selected to true : ","$selected")
-//
-//            } else {
-//                snow.startAnimation(scaleToSmall)
-//                snow.setSelected(selected)
-//                selected = true
-//                Log.d("selected to false : ","$selected")
-//                Log.d("isSeletec?" , "${snow.isSelected}")
-//            }
-            isSelected(imageArray,snow)
-        }
-        sunny.setOnClickListener{
-            isSelected(imageArray,sunny)
-        }
-        rainy.setOnClickListener{
-            isSelected(imageArray,rainy)
-        }
-        cloudy.setOnClickListener{
-            isSelected(imageArray,cloudy)
-        }
     }
 
     private fun isSelected(imageArray:Array<ImageButton>,imageButton:ImageButton){
 
         for ( i in 0..imageArray.size-1 ){
             if(imageArray[i] == imageButton){ // for문을 통해 i번째의 image버튼과 현재 Imagebutton이 같은경우
-                Log.d("if문 내" , "$i ")
                 imageArray[i].setSelected(true)
                 imageArray[i].startAnimation(scaleToBig)
-            }else{                           // for문을 통해 i번째의 image버튼과 현재 Imagebutton이 다른경우 -> 이경우는 어떻게 해도 4번 돔
-                Log.d("If else문" , "$i,${imageArray[i].isSelected}")
+            }else                           // for문을 통해 i번째의 image버튼과 현재 Imagebutton이 다른경우 -> 이경우는 어떻게 해도 4번 돔
                 if(imageArray[i].isSelected) { // 만약 i번째가 선택되어있다면
                     imageArray[i].isSelected = false
                     Log.d("if else if문" , "${i},${imageArray[i].isSelected}")
                     imageArray[i].startAnimation(scaleToSmall)
                 }
-            }
+
         }
-        Log.d("if======================","======================")
+
     }
 
+    override fun onClick(v: View?) {
+        Log.d("on","ok")
+        Log.d("on","${binding.root}")
+
+        try {
+            scaleToBig = AnimationUtils.loadAnimation(this, R.anim.btn_to_big_scale)
+            scaleToSmall = AnimationUtils.loadAnimation(this, R.anim.btn_to_small_scale)
+            val snow = binding.imageButtonSnow
+            val rainy = binding.imageButtonRainy
+            val sunny = binding.imageButtonSunny
+            val cloudy = binding.imageButtonCloudy
+            val windy = binding.imageButtonWindy
+            val happy = binding.imageButtonhappy
+            val good = binding.imageButtonGood
+            val netural = binding.imageButtonNetural
+            val bad = binding.imageButtonBad
+            val confused = binding.imageButtonConfused
+            val angry = binding.imageButtonAngry
+            val nervous = binding.imageButtonNervous
+            val sad = binding.imageButtonSad
+            val sick = binding.imageButtonSick
+
+            val weatherImageArray = arrayOf<ImageButton>(
+                sunny,rainy,snow,cloudy,windy
+            )
+
+            val moodImageArray = arrayOf<ImageButton>(
+                happy,good,netural,bad,confused,angry,nervous,sad,sick
+            )
+            when(v){
+                snow -> isSelected(weatherImageArray,snow)
+                rainy -> isSelected(weatherImageArray,rainy)
+                sunny -> isSelected(weatherImageArray,sunny)
+                cloudy -> isSelected(weatherImageArray,cloudy)
+                windy -> isSelected(weatherImageArray,windy)
+                happy -> isSelected(moodImageArray,happy)
+                good -> isSelected(moodImageArray,good)
+                netural -> isSelected(moodImageArray,netural)
+                bad -> isSelected(moodImageArray,bad)
+                confused -> isSelected(moodImageArray,confused)
+                angry -> isSelected(moodImageArray,angry)
+                nervous -> isSelected(moodImageArray,nervous)
+                sad -> isSelected(moodImageArray,sad)
+                sick -> isSelected(moodImageArray,sick)
+            }
+        }catch (e:NumberFormatException){
+            return
+        }
+
+    }
 
 }
