9b406873206d1c7755ef783d412da1a95c44371f
test <jack-daniel@naver.com>
2023-02-27 15:44:52 +0900

    testcommit parkmunsik

--- app/src/main/java/com/example/picasso/MainActivity.kt
+++ app/src/main/java/com/example/picasso/MainActivity.kt
@@ -25,9 +25,9 @@ import kotlin.math.log
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     var queue: RequestQueue? = null
     lateinit var dataText: TextView
-    var googleSignInClient : GoogleSignInClient?= null
+    var googleSignInClient: GoogleSignInClient? = null
 
-    private val binding:ActivityMainBinding by lazy {
+    private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 //    private val binding:ActivityMainBinding by lazy`` {
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 
         //초기화
-        if(queue == null){
+        if (queue == null) {
             queue = Volley.newRequestQueue(this)
         }
 
@@ -51,59 +51,64 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //버튼 클릭 이벤트
         requestBtn.setOnClickListener {
             //http 호출
-            httpConnection()
-        }
-        binding.DiaryBtn.setOnClickListener{
-            //val intent = Intent(this,DiaryActivity::class.java)
-            try{
-                val intent = Intent(this, StatisticsActivity::class.java)
+            try {
+                val intent = Intent(this, DiaryActivity::class.java)
                 startActivity(intent)
-            }catch (e: Throwable){
+            } catch (e: Throwable) {
                 Log.d("test", "${e}")
             }
         }
-        binding.LoginBtn.setOnClickListener{
+        binding.DiaryBtn.setOnClickListener {
+            //val intent = Intent(this,DiaryActivity::class.java)
+            try {
+                val intent = Intent(this, StatisticsActivity::class.java)
+                startActivity(intent)
+            } catch (e: Throwable) {
+                Log.d("test", "${e}")
+            }
+        }
+        binding.LoginBtn.setOnClickListener {
             val intenttest = Intent(this, LoginPage::class.java)
             startActivity(intenttest)
         }
-        binding.Login2Button.setOnClickListener{
+        binding.Login2Button.setOnClickListener {
             try {
                 val intent = Intent(this, gallery::class.java)
                 startActivity(intent)
-            }catch (e: NumberFormatException){
+            } catch (e: NumberFormatException) {
 
             }
         }
-        binding.logoutButton.setOnClickListener{
+        binding.logoutButton.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             googleSignInClient?.signOut()
 
-            var logoutIntent = Intent (this, MainActivity::class.java)
+            var logoutIntent = Intent(this, MainActivity::class.java)
             logoutIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(logoutIntent)
         }
-        binding.importUserInfo.setOnClickListener{
+        binding.importUserInfo.setOnClickListener {
             var auth = FirebaseAuth.getInstance().currentUser
             var uid = auth?.uid
             var email = auth?.email
             var displayName = auth?.displayName
-            Log.v("myDevice","Build.VERSION.CODENAME = " + Build.VERSION.CODENAME);
-            Log.v("myDevice","Build.VERSION.INCREMENTAL = " + Build.VERSION.INCREMENTAL);
-            Log.v("myDevice","Build.VERSION.RELEASE = " + Build.VERSION.RELEASE);
-            Log.v("myDevice","Build.VERSION.SDK = " + Build.VERSION.SDK);
-            Log.v("myDevice","Build.VERSION.SDK_INT = " + Build.VERSION.SDK_INT);
-            Log.d("UserInfo : ","$auth")
-            Log.d("UserInfo : ","$uid")
-            Log.d("UserInfo : ","$email")
-            Log.d("UserInfo : ","$displayName")
+            Log.v("myDevice", "Build.VERSION.CODENAME = " + Build.VERSION.CODENAME);
+            Log.v("myDevice", "Build.VERSION.INCREMENTAL = " + Build.VERSION.INCREMENTAL);
+            Log.v("myDevice", "Build.VERSION.RELEASE = " + Build.VERSION.RELEASE);
+            Log.v("myDevice", "Build.VERSION.SDK = " + Build.VERSION.SDK);
+            Log.v("myDevice", "Build.VERSION.SDK_INT = " + Build.VERSION.SDK_INT);
+            Log.d("UserInfo : ", "$auth")
+            Log.d("UserInfo : ", "$uid")
+            Log.d("UserInfo : ", "$email")
+            Log.d("UserInfo : ", "$displayName")
             postConnection("$email")
         }
     }
 
-    private fun httpConnection(){
+    private fun httpConnection() {
 
         //데이터 요청할 url 주소
-        val url: String =  "http://10.0.2.2:3000"
+        val url: String = "http://10.0.2.2:3000"
 
         val stringRequest: StringRequest = StringRequest(
             Request.Method.GET,
@@ -121,24 +126,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         queue?.add(stringRequest)
     }
 
-    private fun postConnection(whatIwant:String){
+    private fun postConnection(whatIwant: String) {
         val url: String = "http://10.0.2.2:3000"
 
         val request = object : StringRequest(
             Request.Method.POST,
             url,
-            Response.Listener {
-                response ->
+            Response.Listener { response ->
                 dataText.text = "Response: $response"
-            }, Response.ErrorListener {
-                error ->
+            }, Response.ErrorListener { error ->
                 dataText.text = "Error: $error"
             }) {
             // request 시 key, value 보낼 때
             override fun getParams(): Map<String, String> {
-                val params = mutableMapOf<String,String>()
+                val params = mutableMapOf<String, String>()
                 params["email"] = "$whatIwant"
-                Log.d("params : ","$params")
+                Log.d("params : ", "$params")
                 return params
             }
 
